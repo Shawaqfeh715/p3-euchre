c39284a873a6ebe4c01a320eb02bc10faf1e3d3f
UsmanCh1226 <usmanch@umich.edu>
2026-02-08 18:13:56 -0500

    non member functions with led card

--- starter-files 2/Card.cpp
+++ starter-files 2/Card.cpp
@@ -158,6 +158,10 @@ bool Card_less(const Card &a, const Card &b, Suit trump) {
     assert(false);
 }
 
+bool Card_less(const Card &a, const Card &b, const Card &led_card, Suit trump) {
+    assert(false);
+}
+
 
 // NOTE: We HIGHLY recommend you check out the operator overloading
 // tutorial in the project spec before implementing
