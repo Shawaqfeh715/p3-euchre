adf1b7b48f536a386125042bb7224493e9282591
UsmanCh1226 <usmanch@umich.edu>
2026-02-11 21:51:36 -0500

    a is trump or b is trump or compare both normally

--- starter-files 2/Card.cpp
+++ starter-files 2/Card.cpp
@@ -241,3 +241,15 @@ bool Card_less(const Card &a, const Card &b, const Card &led_card, Suit trump) {
     // 2. If neither is trump, check for the led suit
     Suit led_suit = led_card.get_suit(trump);
     // using get_suit because the led card could be Left Bower
+
+    if (b.get_suit(trump) == led_suit && a.get_suit(trump) != led_suit) {
+        return true; // b follows suit, a doesn't. a is less.
+    }
+
+    if (a.get_suit(trump) == led_suit && b.get_suit(trump) != led_suit) {
+        return false; // a follows suit, b doesn't. a is not less.
+    }
+
+    // 3. Otherwise, compare normally
+    return a < b;
+}
