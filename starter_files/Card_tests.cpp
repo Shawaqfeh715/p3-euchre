0652befac53d56f11e847a911b96167fd7a53313
UsmanCh1226 <usmanch@umich.edu>
2026-02-12 22:47:23 -0500

    testing identity of left bower

--- starter_files/Card_tests.cpp
+++ starter_files/Card_tests.cpp
@@ -11,5 +11,17 @@ TEST(test_card_ctor) {
     ASSERT_EQUAL(HEARTS, c.get_suit());
 }
 
+TEST(identity_of_left_bower) {
+    Card jack_of_diamonds(JACK, DIAMONDS);
+    Suit trump = HEARTS;
+
+    // edge case: suit should be reported as hearts even though it's a DIAMOND
+    ASSERT_TRUE(jack_diamonds.is_left_bower(trump));
+    ASSERT_TRUE(jack_diamonds.is_trump(trump));
+    ASSERT_EQUAL(jack_diamonds.get_suit(trump), HEARTS);
+
+    // if it's not a trump, it's a normal diamond
+    ASSERT_TRUE(jack_of_diamonds.get_suit() == DIAMONDS);
+}
 // Add more test cases here
 TEST_MAIN()
