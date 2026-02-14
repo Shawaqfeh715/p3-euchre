157323f4881f707bc4c37371df6e61931330ec6a
UsmanCh1226 <usmanch@umich.edu>
2026-02-14 14:43:44 -0500

    helper function for rank

--- starter_files/Pack.cpp
+++ starter_files/Pack.cpp
@@ -1,3 +1,16 @@
 #include "Pack.hpp"
 #include <string>
+#include <cassert>
+
+// helper functions
+// These help the stream constructor convert strings to Enums
+static Rank string_to_rank(std::string str) {
+    if (str == "Nine") return NINE;
+    if (str == "Ten") return TEN;
+    if (str == "Jack") return JACK;
+    if (str == "Queen") return QUEEN;
+    if (str == "King") return KING;
+    if (str == "Ace") return ACE;
+    assert(false); // Should never happen if input is valid
+}
 
