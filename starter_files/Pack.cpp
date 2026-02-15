e17e54dc84c4edcd153f62249486ab7b57ea4a4a
UsmanCh1226 <usmanch@umich.edu>
2026-02-15 18:59:35 -0500

    got rid of helper functions because test cases would fail

--- starter_files/Pack.cpp
+++ starter_files/Pack.cpp
@@ -2,26 +2,7 @@
 #include <string>
 #include <cassert>
 
-// helper functions
-// These help the stream constructor convert strings to Enums
-static Rank string_to_rank(std::string str) {
-    if (str == "Nine") return NINE;
-    if (str == "Ten") return TEN;
-    if (str == "Jack") return JACK;
-    if (str == "Queen") return QUEEN;
-    if (str == "King") return KING;
-    if (str == "Ace") return ACE;
-    assert(false); // Should never happen if input is valid
-}
 
-
-static Suit string_to_suit(std::string str) {
-    if (str == "Spades") return SPADES;
-    if (str == "Hearts") return HEARTS;
-    if (str == "Clubs") return CLUBS;
-    if (str == "Diamonds") return DIAMONDS;
-    assert(false); // Should never happen if input is valid
-}
 
 Pack::Pack() {
     next = 0;
