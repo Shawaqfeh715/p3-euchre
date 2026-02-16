f5bec4a4283ed935371938cfc74915c076f07ef1
UsmanCh1226 <usmanch@umich.edu>
2026-02-16 15:42:33 -0500

    data parser test case

--- starter_files/Pack_tests.cpp
+++ starter_files/Pack_tests.cpp
@@ -1,6 +1,6 @@
 #include "Pack.hpp"
 #include "unit_test_framework.hpp"
-
+#include <sstream>
 #include <iostream>
 
 using namespace std;
@@ -42,5 +42,26 @@ TEST(test_pack_reset) {
     
     ASSERT_EQUAL(first_card, reset_card);
 }
+
+
+TEST(test_pack_stream_reader) {
+    // A tiny pack of cards in a string
+    std::string data = "Nine of Spades\nTen of Spades\nJack of Spades\n"
+                       "Queen of Spades\nKing of Spades\nAce of Spades\n"
+                       
+                       "Nine of Hearts\nTen of Hearts\nJack of Hearts\n"
+                       "Queen of Hearts\nKing of Hearts\nAce of Hearts\n"
+                       "Nine of Clubs\nTen of Clubs\nJack of Clubs\n"
+                       "Queen of Clubs\nKing of Clubs\nAce of Clubs\n"
+                       "Nine of Diamonds\nTen of Diamonds\nJack of Diamonds\n"
+                       "Queen of Diamonds\nKing of Diamonds\nAce of Diamonds\n";
+    
+    std::istringstream iss(data);
+    Pack pack(iss);
+    
+    Card first = pack.deal_one();
+    ASSERT_EQUAL(first, Card(NINE, SPADES));
+}
+
 
 TEST_MAIN()
