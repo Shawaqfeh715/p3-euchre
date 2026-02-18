473a3937c00acdcc84d97040b4c894e034fddbdb
UsmanCh1226 <usmanch@umich.edu>
2026-02-18 09:46:24 -0500

    initial logic for make_trump function

--- starter_files/player.cpp
+++ starter_files/player.cpp
@@ -20,4 +20,39 @@ void add_card(const Card &c) override {
   }
 
 
+  bool make_trump(const Card &upcard, bool is_dealer,
+                  int round, Suit &order_up_suit) const override {
+    int count = 0;
+    Suit candidate;
+
+    if (round == 1) {
+      candidate = upcard.get_suit();
+      for (const auto &c : hand) {
+        if (c.is_trump(candidate) && c.is_face_or_ace()) {
+          count++;
+        }
+      }
+      if (count >= 2) {
+        order_up_suit = candidate;
+        return true;
+      }
+    } 
+    else if (round == 2) {
+      candidate = Suit_next(upcard.get_suit());
+      if (is_dealer) { // Screw the dealer
+        order_up_suit = candidate;
+        return true;
+      }
+      for (const auto &c : hand) {
+        if (c.is_trump(candidate) && c.is_face_or_ace()) {
+          count++;
+        }
+      }
+      if (count >= 1) {
+        order_up_suit = candidate;
+        return true;
+      }
+    }
+    return false;
+  }
 };
