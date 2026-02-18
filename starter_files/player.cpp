a96e1dad7b896fd6a83269e6ce998ee9d5b0fdcb
UsmanCh1226 <usmanch@umich.edu>
2026-02-18 07:26:57 -0500

    added first few members to simple player class

--- starter_files/player.cpp
+++ starter_files/player.cpp
@@ -5,3 +5,19 @@
 
 using namespace std;
 
+// Simpler player implementation
+class SimplePlayer : public Player {
+public:
+SimplePlayer(const string &name) : name(name) {}
+
+const string & get_name() const override {
+    return name;
+}
+
+void add_card(const Card &c) override {
+    assert(hand.size() < MAX_HAND_SIZE);
+    hand.push_back(c);
+  }
+
+
+}
