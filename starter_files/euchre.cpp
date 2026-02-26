658455e8ae8099fa9af2b49ab85d35a61466cdd6
UsmanCh1226 <usmanch@umich.edu>
2026-02-25 22:43:02 -0500

    first attempt at full euchre.cpp

--- starter_files/euchre.cpp
+++ starter_files/euchre.cpp
@@ -23,7 +23,7 @@ public:
             play_hand();
             
             cout << players[0]->get_name() << " and " << players[2]->get_name()
-                 << " have " << team1_score << " points" << endl;
+                 << " have " << team1_score << " points" << endl << endl;
             cout << players[1]->get_name() << " and " << players[3]->get_name()
                  << " have " << team2_score << " points" << endl << endl;
 
@@ -43,25 +43,22 @@ private:
     Pack pack;
     int points_to_win;
     bool shuffle;
-    int team1_score; // Team 0: Players 0 & 2
-    int team2_score; // Team 1: Players 1 & 3
+    int team1_score;
+    int team2_score;
     int dealer_idx;
 
     void deal(Card &upcard) {
         if (shuffle) { pack.shuffle(); } 
         else { pack.reset(); }
 
-        // Exact 3-2-3-2 then 2-3-2-3 pattern from specs
         int batches[] = {3, 2, 3, 2, 2, 3, 2, 3};
         int current_player = (dealer_idx + 1) % 4;
-        
         for (int i = 0; i < 8; ++i) {
             for (int j = 0; j < batches[i]; ++j) {
                 players[current_player]->add_card(pack.deal_one());
             }
             current_player = (current_player + 1) % 4;
         }
-        
         upcard = pack.deal_one();
         cout << players[dealer_idx]->get_name() << " deals" << endl;
         cout << upcard << " turned up" << endl;
@@ -73,7 +70,6 @@ private:
         Suit trump;
         int orderer_idx = -1;
 
-        // Round 1
         for (int i = 1; i <= 4; ++i) {
             int current = (dealer_idx + i) % 4;
             if (players[current]->make_trump(upcard, (current == dealer_idx), 1, trump)) {
@@ -85,7 +81,6 @@ private:
             cout << players[current]->get_name() << " passes" << endl;
         }
 
-        // Round 2
         if (orderer_idx == -1) {
             for (int i = 1; i <= 4; ++i) {
                 int current = (dealer_idx + i) % 4;
@@ -101,13 +96,12 @@ private:
     }
 
     void play_tricks(Suit trump, int orderer_idx) {
-        int team1_tricks = 0, team2_tricks = 0;
+        int t1_t = 0, t2_t = 0;
         int leader_idx = (dealer_idx + 1) % 4;
 
         for (int i = 0; i < 5; ++i) {
             Card led_card = players[leader_idx]->lead_card(trump);
             cout << led_card << " led by " << players[leader_idx]->get_name() << endl;
-            
             Card highest = led_card;
             int winner_idx = leader_idx;
 
@@ -115,7 +109,6 @@ private:
                 int current = (leader_idx + j) % 4;
                 Card played = players[current]->play_card(led_card, trump);
                 cout << played << " played by " << players[current]->get_name() << endl;
-                
                 if (Card_less(highest, played, led_card, trump)) {
                     highest = played;
                     winner_idx = current;
@@ -123,89 +116,58 @@ private:
             }
             cout << players[winner_idx]->get_name() << " takes the trick" << endl << endl;
             leader_idx = winner_idx;
-            (winner_idx % 2 == 0) ? team1_tricks++ : team2_tricks++;
+            (winner_idx % 2 == 0) ? t1_t++ : t2_t++;
         }
-        score_hand(team1_tricks, team2_tricks, orderer_idx);
-    }
-
-    void score_hand(int t1_t, int t2_t, int orderer) {
-        bool team1_ordered = (orderer == 0 || orderer == 2);
+        
+        bool t1_ord = (orderer_idx == 0 || orderer_idx == 2);
         if (t1_t > t2_t) {
             cout << players[0]->get_name() << " and " << players[2]->get_name() << " win the hand" << endl;
-            if (team1_ordered) {
+            if (t1_ord) {
                 if (t1_t == 5) { cout << "march!" << endl; team1_score += 2; }
                 else { team1_score += 1; }
-            } else { 
-                cout << "euchred!" << endl; 
-                team1_score += 2; 
-            }
+            } else { cout << "euchred!" << endl; team1_score += 2; }
         } else {
             cout << players[1]->get_name() << " and " << players[3]->get_name() << " win the hand" << endl;
-            if (!team1_ordered) {
+            if (!t1_ord) {
                 if (t2_t == 5) { cout << "march!" << endl; team2_score += 2; }
                 else { team2_score += 1; }
-            } else { 
-                cout << "euchred!" << endl; 
-                team2_score += 2; 
-            }
+            } else { cout << "euchred!" << endl; team2_score += 2; }
         }
     }
 };
 
 int main(int argc, char **argv) {
-    // 1. Check Argument Count
     if (argc != 12) {
         cout << "Usage: euchre.exe PACK_FILENAME [shuffle|noshuffle] "
              << "POINTS_TO_WIN NAME1 TYPE1 NAME2 TYPE2 NAME3 TYPE3 "
              << "NAME4 TYPE4" << endl;
         return 1;
     }
-
-    // 2. Extract and Validate Arguments
-    string pack_filename = argv[1];
-    string shuffle_arg = argv[2];
-    int points_to_win = stoi(argv[3]);
-
-    if ((shuffle_arg != "shuffle" && shuffle_arg != "noshuffle") || 
-        (points_to_win < 1 || points_to_win > 100)) {
+    string pack_fn = argv[1];
+    string shuff_arg = argv[2];
+    int p_win = stoi(argv[3]);
+    if ((shuff_arg != "shuffle" && shuff_arg != "noshuffle") || (p_win < 1 || p_win > 100)) {
         cout << "Usage: euchre.exe PACK_FILENAME [shuffle|noshuffle] "
              << "POINTS_TO_WIN NAME1 TYPE1 NAME2 TYPE2 NAME3 TYPE3 "
              << "NAME4 TYPE4" << endl;
         return 1;
     }
-
-    // 3. Open Pack File
-    ifstream fin(pack_filename);
-    if (!fin.is_open()) {
-        cout << "Error opening " << pack_filename << endl;
-        return 1;
-    }
-
-    // 4. Create Players and Validate Types
+    ifstream fin(pack_fn);
+    if (!fin.is_open()) { cout << "Error opening " << pack_fn << endl; return 1; }
     vector<Player*> players;
     for (int i = 4; i < 12; i += 2) {
-        string name = argv[i];
-        string type = argv[i+1];
+        string name = argv[i], type = argv[i+1];
         if (type != "Simple" && type != "Human") {
-            cout << "Usage: euchre.exe PACK_FILENAME [shuffle|noshuffle] "
-                 << "POINTS_TO_WIN NAME1 TYPE1 NAME2 TYPE2 NAME3 TYPE3 "
-                 << "NAME4 TYPE4" << endl;
-            for (Player* p : players) delete p;
-            return 1;
+            cout << "Usage: euchre.exe PACK_FILENAME [shuffle|noshuffle] POINTS_TO_WIN NAME1 TYPE1 NAME2 TYPE2 NAME3 TYPE3 NAME4 TYPE4" << endl;
+            for (Player* p : players) delete p; return 1;
         }
         players.push_back(Player_factory(name, type));
     }
-
-    // 5. Output Command Line Arguments
-    for (int i = 0; i < argc; ++i) { cout << argv[i] << (i == argc - 1 ? "" : " "); }
-    cout << " " << endl; // Spec says space at the end
-
-    // 6. Run Game
+    for (int i = 0; i < argc; ++i) { cout << argv[i] << " "; }
+    cout << endl;
     Pack pack(fin);
-    bool do_shuffle = (shuffle_arg == "shuffle");
-    Game game(players, pack, points_to_win, do_shuffle);
+    Game game(players, pack, p_win, (shuff_arg == "shuffle"));
     game.play();
-
-    // 7. Cleanup
     for (Player* p : players) { delete p; }
     return 0;
+}
